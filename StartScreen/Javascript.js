4aeb2b72c225a93500c0daa7392de1fdca460747
gonzalez1ucy <glucy822@gmail.com>
2019-07-12 17:45:41 +0000

    saved

---
+++ StartScreen/Javascript.js
@@ -0,0 +1,20 @@
+function region(){
+document.getElementsByClassName("regionSelect")[1].click();	}
+ region();
+function tabs(){
+document.getElementsByClassName("strong tab")[0].click();		}
+tabs();
+function get(){
+document.getElementsByClassName("os_block  win7")[0].click();   }
+get();
+function vm(){
+document.getElementsByClassName("vmType")[2].click();		}
+vm();
+function checkmark(){
+document.getElementsByClassName("circleOpt")[2].click();  }
+checkmark();
+function final(){
+document.getElementsByClassName("hollow medium whiteGlo")[0].click();  }
+final();
+//Make the machine verify
+alert("success");
