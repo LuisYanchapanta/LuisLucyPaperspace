63671da9a29905fd0b7619fe4e7b8e128ab1e3c7
Lucy Gonzalez <lucygonzalez@Paperspaces-MacBook-Air.local>
2019-07-15 14:22:37 -0400

    wip feat: machine create scenario test

--- StartScreen/Javascript.js
+++ StartScreen/Javascript.js
@@ -1,23 +1,44 @@
 function region(){
-document.getElementsByClassName("regionSelect")[1].click();	}
+document.getElementsByClassName("regionSelect")[1].click();	
+}
  region();
 function tabs(){
-document.getElementsByClassName("strong tab")[0].click();		}
+document.getElementsByClassName("strong tab")[0].click();		
+}
 tabs();
 function get(){
-document.getElementsByClassName("os_block  win7")[0].click();   }
+document.getElementsByClassName("os_block  win7")[0].click();  
+ }
 get();
 function vm(){
-document.getElementsByClassName("vmType")[2].click();		}
+document.getElementsByClassName("vmType")[2].click();		
+}
 vm();
 function checkmark(){
-document.getElementsByClassName("circleOpt")[2].click();  }
+document.getElementsByClassName("circleOpt")[2].click();  
+}
 checkmark();
 function final(){
-document.getElementsByClassName("hollow medium whiteGlo")[0].click();  }
+document.getElementsByClassName("hollow medium whiteGlo")[0].click(); 
+ }
 final();
-//Make the machine verify
- if (confirm("On/Ready")) {
-  txt = "VM is running!";
+function redirect() {
+  location.replace("https://www.w3schools.com")
 }
+Timer timer = new Timer();
+timer.schedule(new TimerTask() {
 
+        @Override
+        public void run() {
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    label.setText("");
+                }
+            });
+
+        }
+    }, 3000);
+    function redirect() { 
+      location.replace("https://www.paperspace.com/console/machines")
+}
