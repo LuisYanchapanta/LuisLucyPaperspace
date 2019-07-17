4f1657bfd3d52035d63d6a7d318bc8260b38fff7
Lucy Gonzalez <glucy822@gmail.com>
2019-07-17 10:27:29 -0400

    addressing the comments

--- StartScreen/Javascript.js
+++ StartScreen/Javascript.js
@@ -1,40 +1,35 @@
 function region(){
-document.getElementsByClassName("regionSelect")[1].click();	
-}
- region();
-function tabs(){
-document.getElementsByClassName("strong tab")[0].click();		
-}
-tabs();
-function get(){
-document.getElementsByClassName("os_block  win7")[0].click();  
- }
-get();
-function vm(){
-document.getElementsByClassName("vmType")[2].click();		
-}
-vm();
-function checkmark(){
-document.getElementsByClassName("circleOpt")[2].click();  
-}
-checkmark();
-function final(){
-document.getElementsByClassName("hollow medium whiteGlo")[0].click(); 
- }
-final();
-function ready() {
-	document.getElementsByClassName("state-on")[1].innerText.includes("On/Ready");
-	return (true);
-}
-ready();
- //if (ready === true ){setTimeout(function(){
-    window.location.href = 'https://www.paperspace.com/console/machines';
-    }, 120000);}
-    
-    
-    // setTimeout(function(){
-    window.location.href = 'https://www.paperspace.com/console/machines';
-    }, 120000);
-
+    document.getElementsByClassName("regionSelect")[1].click();	
+    }
+     region();
+    function Tiletabs(){
+    document.getElementsByClassName("strong tab")[0].click();		
+    }
+    Tiletabs();
+    function get(){
+    document.getElementsByClassName("os_block  win7")[0].click();  
+     }
+    get();
+    function vm(){
+    document.getElementsByClassName("vmType")[2].click();		
+    }
+    vm();
+    function checkmark(){
+    document.getElementsByClassName("circleOpt")[2].click();  
+    }
+    checkmark();
+    function final(){
+    document.getElementsByClassName("hollow medium whiteGlo")[0].click(); 
+     }
+    final();
+    function ready() {
+    document.getElementsByClassName("state-on")[3].innerText.includes("On/Ready");
+        alert ("Ready!");
+    }
+    ready();
+        //if (ready === true ){setTimeout(function(){
+        //window.location.href = 'https://www.paperspace.com/console/machines';
+        //}, 120000);}
+        // this part of the code is still a WIP.
     
 
